cb2d59bc43b24bd5aea9a63471ad60ae09009c7b
Robert Tian <74839761+Robertian23@users.noreply.github.com>
2022-10-06 18:37:26 +0000

    add problems

---
+++ labs/lab03.py
@@ -0,0 +1,198 @@
+# lab03
+
+
+# disc03: https://inst.eecs.berkeley.edu/~cs61a/su22/disc/disc03/
+
+
+def multiply(m, n): # q1
+    """ Takes two positive integers and returns their product using recursion.
+    >>> multiply(5, 3)
+    15
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def is_prime(n): # q2
+    """Returns True if n is a prime number and False otherwise.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def hailstone(n): # q3
+    """Print out the hailstone sequence starting at n, and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def merge(n1, n2): # q4
+    """ Merges two numbers by digit in decreasing order
+    >>> merge(31, 42)
+    4321
+    >>> merge(21, 0)
+    21
+    >>> merge (21, 31) 
+    3211
+    """
+    "*** YOUR CODE HERE ***"
+
+
+# lab03: https://inst.eecs.berkeley.edu/~cs61a/su22/lab/lab03/
+
+
+def summation(n, term): # q5
+    """Return the sum of numbers 1 through n (including n) wíth term applied to each number.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    """
+    assert n >= 1
+    "*** YOUR CODE HERE ***"
+
+ 
+def paths(m, n): # q6
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
+# hw03: https://inst.eecs.berkeley.edu/~cs61a/su22/hw/hw03/
+
+
+def num_eights(pos): # q7
+    """Returns the number of times 8 appears as a digit of pos.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def pingpong(n): # q8, DO NOT use variable assignment/reassignment statements
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def get_larger_coin(coin): # don't edit function
+    """Returns the next larger coin in order.
+    >>> get_larger_coin(1)
+    5
+    >>> get_larger_coin(5)
+    10
+    >>> get_larger_coin(10)
+    25
+    >>> get_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+def get_smaller_coin(coin): # don't edit function
+    """Returns the next smaller coin in order.
+    >>> get_smaller_coin(25)
+    10
+    >>> get_smaller_coin(10)
+    5
+    >>> get_smaller_coin(5)
+    1
+    >>> get_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+def count_coins(change): # q9, use get_smaller_coin or get_larger_coin
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
