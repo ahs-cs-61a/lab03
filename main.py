604e7d9e8f06e6c9536427b0e0a666612c14114c
Robert Tian <roberttian0523@gmail.com>
2023-01-12 22:04:59 -0800

    minor

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from labs.lab03 import *
 from labs.lab03_wwpd import *
 
 # WWPD? questions:  
-# un-comment one-by-one and complete question set
+# un-comment to complete question set
 
 # wwpd_virfib_sq()
